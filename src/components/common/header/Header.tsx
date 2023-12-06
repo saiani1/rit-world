ca11bc16cccb3621a315371c9d8ffd822cce01ee
saiani1 <saiani@naver.com>
2023-12-06 16:55:46 +0900

    Update Category, Category List 퍼블리싱 완료

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div>
-      <header>아이바오의 미니홈피</header>
-      <p>http://localhost:3000</p>
+    <div className="flex justify-between">
+      <header className="font-bold text-purple-700">아이바오의 미니홈피</header>
+      <p className="text-sm text-gray-600">http://localhost:3000</p>
     </div>
   );
 };
