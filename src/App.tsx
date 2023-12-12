fa141c7fed57e0bcadf397eb5f9ad2cf41f05150
saiani1 <saiani@naver.com>
2023-12-12 17:57:45 +0900

    toast라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import HomeScreen from "./routes/home/HomeScreen";
 import SignInScreen from "./routes/signin/SignInScreen";
-import { Outlet } from "react-router-dom";
 
 const App = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -9,6 +11,14 @@ const App = () => {
     <div className="flex justify-center items-center h-screen bg-slate-200">
       {/* {isLogin ? <HomeScreen /> : <SignInScreen />} */}
       <Outlet />
+      <Toaster
+        containerStyle={{
+          top: 20,
+        }}
+        toastOptions={{
+          duration: 2000,
+        }}
+      />
     </div>
   );
 };
